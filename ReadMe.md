72fb240a3c023943398f0b7dce0169c253938501
kavoshnik <hikkan008@gmail.com>
2025-04-09 19:17:43 +0700

    new file:   ReadMe.md 	new file:   types.cs

---
+++ ReadMe.md
@@ -0,0 +1,17 @@
+# This is ReadMe for it guide
+
+For compile csharp code use this:
+
+>> You need mono for use this.
+
+```bash
+mcs FileName.cs
+```
+
+For run compile file use this:
+
+```bash
+mono FileName.exe
+```
+
+>> P.S. You can run use this method in Linux, it work)
---
+++ types.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace types {
+    class Types {
+        static void Main() {
+            Console.Write("This is a types guide for C#:\n");
+            // Console.ReadLine(); Получение ввода
+            Console.Write("C# has this types:\n");
+        }
+    }
+}
